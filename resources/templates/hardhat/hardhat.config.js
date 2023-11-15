78dafb7bbb77be1bce425ec5b2ea26af4dd0f6b6
hazelcaus <hazelcaus@gmail.com>
2023-11-15 13:54:26 +0530

    Update hardhat template

--- resources/templates/hardhat/hardhat.config.js
+++ resources/templates/hardhat/hardhat.config.js
@@ -1,26 +1,32 @@
 require("@nomiclabs/hardhat-waffle")
-require("@vechain.energy/hardhat-thor")
-const SoloDefault = require("thor-builtin").SoloDefault
+require("@nomiclabs/hardhat-web3")
+require("@vechain/hardhat-web3")
+const {
+    VECHAIN_URL_SOLO,
+    VECHAIN_URL_TESTNET,
+    VECHAIN_URL_MAINNET,
+    VECHAIN_DEFAULT_MNEMONIC
+  } = require("@vechain/hardhat-vechain")
 
-const PRIVATE_KEY = "0x80b97e2ecfab8b1c78100c418328e8a88624e3d19928ec791a8a51cdcf01f16f"
 module.exports = {
     solidity: "0.8.4",
     networks: {
         // This is still unstable and not thoroughly tested
         development: {
-            url: "http://127.0.0.1:8669",
-            privateKey: SoloDefault.privKeys,
+            url: VECHAIN_URL_SOLO,
+            mnemonic: [VECHAIN_DEFAULT_MNEMONIC],
         },
-        thor_mainnet: {
-            url: "https://mainnet.veblocks.net",
-            privateKey: PRIVATE_KEY,
-            blockGasLimit: 10000000,
+        vechain_mainnet: {
+            url: VECHAIN_URL_MAINNET,
+            mnemonic: [VECHAIN_DEFAULT_MNEMONIC],
         },
-        thor_testnet: {
-            url: "https://testnet.veblocks.net",
-            privateKey: PRIVATE_KEY,
-            delegateUrl: "https://sponsor-testnet.vechain.energy/by/1",
-            blockGasLimit: 10000000,
+        vechain_testnet: {
+            url: VECHAIN_URL_TESTNET,
+            mnemonic: [VECHAIN_DEFAULT_MNEMONIC],
+            delegate: {
+                url: "${feeDelegationServiceUrl}",
+                signer: "${optionalSigner}"
+            }
         },
     },
 }
